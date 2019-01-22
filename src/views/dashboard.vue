96574a5803020879153a388743239ba6051f749a
weekbin <18717744742@163.com>
2019-01-22 13:20:44 +0800

    create dashboard

---
+++ src/views/dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+	<div style="position: relative">
+		<toolbar @childEvent="changeInfo"></toolbar>
+		<toollist :showInfo="showInfo"></toollist>
+		<Content :showInfo="showInfo"></Content>
+	</div>
+</template>
+
+<script>
+import Toolbar from "@/components/dashboard/toolbar";
+import Toollist from "@/components/dashboard/toollist";
+import Content from "@/components/dashboard/content";
+export default {
+	name: "dashboard",
+	components: {Content, Toollist, Toolbar},
+	data(){
+		return{
+			showInfo:false
+		}
+	},
+	methods:{
+		changeInfo(data){
+			this.showInfo = data
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
