cfbecac239da0d7625149022193446df16e9f8ed
weekbin <18717744742@163.com>
2019-01-23 22:04:27 +0800

    Dynamic data

--- src/components/dashboard/content/news-management.vue
+++ src/components/dashboard/content/news-management.vue
@@ -1,101 +1,191 @@
 <template>
 	<div style="margin: 20px">
-		<md-table md-card>
-			<md-table-toolbar>
-				<h1 class="md-title">Users</h1>
-			</md-table-toolbar>
-			
+		<md-card style="padding: 30px;margin-bottom: 20px">
+			<h3>请输入检索条件</h3>
+
+			<div>
+				<div class="md-layout md-gutter">
+					<div class="md-layout-item md-size-15">
+						<md-field>
+							<label for="status">Status</label>
+							<md-select v-model="status" name="status" id="status" md-dense>
+								<md-option value=0>未批注</md-option>
+								<md-option value="1">已批注</md-option>
+							</md-select>
+						</md-field>
+					</div>
+
+					<div class="md-layout-item md-size-15">
+						<md-field>
+							<label for="site">Site</label>
+							<md-select v-model="site" name="site" id="site" md-dense>
+								<md-option v-for="i in siteList" :key="i.id" :value="i.id">{{i.name}}</md-option>
+							</md-select>
+						</md-field>
+					</div>
+
+					<div class="md-layout-item md-size-15">
+						<md-field>
+							<label for="limit">limit</label>
+							<md-input v-model="limit" name="limit" id="limit" autocomplete="20"/>
+						</md-field>
+					</div>
+
+					<div class="md-layout-item md-size-10">
+						<md-button class="md-raised md-primary" style="width: 100%" @click="getNews">查询</md-button>
+					</div>
+
+					<div class="md-layout-item md-size-10">
+						<md-button class="md-raised md-primary" style="width: 100%" @click="alterMode = !alterMode">{{alterModeString}}</md-button>
+					</div>
+					
+					<div class="md-layout-item md-size-10">
+						<md-button class="md-raised md-primary" style="width: 100%" @click="patchNews">批量修改</md-button>
+					</div>
+				</div>
+			</div>
+		</md-card>
+
+		<md-card>
+			<md-table>
+				<md-table-toolbar>
+					<h1 class="md-title">News</h1>
+				</md-table-toolbar>
+			</md-table>
+		</md-card>
+
+		<md-table md-card :style="{'max-height': this.$common.ScreenHeight(380)}" v-if="!alterMode">
+
 			<md-table-row>
 				<md-table-head md-numeric>ID</md-table-head>
-				<md-table-head>Name</md-table-head>
-				<md-table-head>Email</md-table-head>
-				<md-table-head>Gender</md-table-head>
-				<md-table-head>Job Title</md-table-head>
-				<md-table-head>领域</md-table-head>
+				<md-table-head>Title</md-table-head>
+				<md-table-head>Link</md-table-head>
+				<md-table-head>Click</md-table-head>
+				<md-table-head>Status</md-table-head>
+				<md-table-head>Site</md-table-head>
+				<md-table-head>Field</md-table-head>
 			</md-table-row>
-			
-			<md-table-row v-for="i in 3">
-				<md-table-cell md-numeric>1</md-table-cell>
-				<md-table-cell>Shawna Dubbin</md-table-cell>
-				<md-table-cell>sdubbin0@geocities.com</md-table-cell>
-				<md-table-cell>Male</md-table-cell>
-				<md-table-cell>Assistant Media Planner</md-table-cell>
+
+			<md-table-row v-for="i in news" :key="i.id">
+				<md-table-cell md-numeric>{{i.id}}</md-table-cell>
+				<md-table-cell>{{i.title}}</md-table-cell>
+				<md-table-cell><a :href="i.link" target="_blank">{{i.link}}</a></md-table-cell>
+				<md-table-cell>{{i.click}}</md-table-cell>
+				<md-table-cell>{{i.status}}</md-table-cell>
+				<md-table-cell>{{i.site_id}}</md-table-cell>
+				<md-table-cell>{{i.selectedFields | ifSelected}}</md-table-cell>
+			</md-table-row>
+		</md-table>
+
+		<md-table md-card :style="{'max-height': this.$common.ScreenHeight(380)}" v-if="alterMode">
+
+			<md-table-row>
+				<md-table-head md-numeric>ID</md-table-head>
+				<md-table-head>Title</md-table-head>
+				<md-table-head>Link</md-table-head>
+				<md-table-head>Click</md-table-head>
+				<md-table-head>Status</md-table-head>
+				<md-table-head>Site</md-table-head>
+				<md-table-head>Field</md-table-head>
+			</md-table-row>
+
+			<md-table-row v-for="i in news" :key="i.id">
+				<md-table-cell md-numeric>{{i.id}}</md-table-cell>
+				<md-table-cell>{{i.title}}</md-table-cell>
+				<md-table-cell><a :href="i.link">{{i.link}}</a></md-table-cell>
+				<md-table-cell>{{i.click}}</md-table-cell>
+				<md-table-cell>{{i.status}}</md-table-cell>
+				<md-table-cell>{{i.site_id}}</md-table-cell>
 				<md-table-cell>
-					<div class="md-layout md-gutter">
-						<div class="md-layout-item">
-							<md-field>
-								<label for="movies">Movies</label>
-								<md-select v-model="selectedMovies" name="movies" id="movies" multiple>
-									<md-option value="fight-club">Fight Club</md-option>
-									<md-option value="godfather">Godfather</md-option>
-									<md-option value="godfather-ii">Godfather II</md-option>
-									<md-option value="godfather-iii">Godfather III</md-option>
-									<md-option value="godfellas">Godfellas</md-option>
-									<md-option value="pulp-fiction">Pulp Fiction</md-option>
-									<md-option value="scarface">Scarface</md-option>
-								</md-select>
-							</md-field>
-							
-							<!--<div>-->
-								<!--<strong>Selected movies:</strong>-->
-								<!--{{ selectedMovies }}-->
-							<!--</div>-->
-						</div>
-					</div>
+					<md-field>
+						<label for="field">fields</label>
+						<md-select v-model="i.selectedFields" name="field" id="field" multiple>
+							<md-option v-for="i in fieldList" :key="i.id" :value="i.field">{{i.field}}</md-option>
+						</md-select>
+					</md-field>
 				</md-table-cell>
 			</md-table-row>
 		</md-table>
+		<md-dialog-alert
+				:md-active.sync="second"
+				md-title="Post created!"
+				md-content="修改成功" />
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
 	name: "news-management",
+	props:["fieldList"],
 	data: () => ({
-		users: [
-			{
-				id: 1,
-				name: 'Shawna Dubbin',
-				email: 'sdubbin0@geocities.com',
-				gender: 'Male',
-				title: 'Assistant Media Planner'
-			},
-			{
-				id: 2,
-				name: 'Odette Demageard',
-				email: 'odemageard1@spotify.com',
-				gender: 'Female',
-				title: 'Account Coordinator'
-			},
-			{
-				id: 3,
-				name: 'Lonnie Izkovitz',
-				email: 'lizkovitz3@youtu.be',
-				gender: 'Female',
-				title: 'Operator'
-			},
-			{
-				id: 4,
-				name: 'Thatcher Stave',
-				email: 'tstave4@reference.com',
-				gender: 'Male',
-				title: 'Software Test Engineer III'
-			},
-			{
-				id: 5,
-				name: 'Clarinda Marieton',
-				email: 'cmarietonh@theatlantic.com',
-				gender: 'Female',
-				title: 'Paralegal'
+		news: [],
+		status:0,
+		site:1,
+		limit:10,
+		sites:null,
+		alterMode:false,
+		second:false,
+		siteList:null
+	}),
+	methods:{
+		getNews(){
+			axios.get('news',{params:{status:this.status, site_id:this.site, limit:this.limit}})
+				.then(response=>{
+					this.news = response.data.publishList.all_news
+					// this.news.forEach(value => {
+					// 	value["selectedFields"] = []
+					// })
+				})
+		},
+		getSites(){
+			axios.get('site').then(response=>{
+				this.siteList = response.data.sites_list
+			})
+		},
+		patchNews(){
+			this.news.forEach(value => {
+				if (value.selectedFields.length > 0){
+					axios.patch('news', {news_id:value.id, field_name_list:value.selectedFields})
+						.then(response=>{
+							this.second = true
+						})
+						.catch(error=>{
+							alert("身份过期")
+						})
+				}
+			})
+			this.alterMode = false
+		}
+	},
+	filters:{
+		ifSelected: function(value){
+			if (value.length === 0){
+				return "数据库没有领域信息"
+			}else {
+				let s = ""
+				for(let i in value){
+					s += value[i] + "、"
+				}
+				return s
 			}
-		],
-		selectedMovies: []
-	})
+		}
+	},
+	computed:{
+		alterModeString(){
+			if (this.alterMode){
+				return "浏览模式"
+			}else {
+				return "修改模式"
+			}
+		}
+	},
+	mounted(){
+		this.getSites()
+	}
 }
 </script>
 
 <style scoped>
-.md-field {
-	max-width: 200px;
-}
+
 </style>
